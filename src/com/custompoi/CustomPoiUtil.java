40086412156e9fb7b7b7ffe1539010f45685e93f
Aditya <Aditya@DESKTOP-R0M715G>
2017-02-27 23:03:41 +0530

    first commit from eclipse

--- src/com/custompoi/CustomPoiUtil.java
+++ src/com/custompoi/CustomPoiUtil.java
@@ -1,27 +1,37 @@
 package com.custompoi;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import com.google.gson.Gson;
+
 public class CustomPoiUtil {
-	private String name;
-	private double lat;
-	private double lon;
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public double getLat() {
-		return lat;
-	}
-	public void setLat(double lat) {
-		this.lat = lat;
-	}
-	public double getLon() {
-		return lon;
-	}
-	public void setLon(double lon) {
-		this.lon = lon;
+	
+	public static int addPoi(CustomPoiVO customPoiVO){
+		
+		Gson gson = new Gson();
+		JSONObject poiJson;
+		JSONObject poiJson1 = new JSONObject();
+		try {
+			poiJson = (JSONObject)(new JSONParser().parse(gson.toJson(customPoiVO)));
+			poiJson1.put("ab", poiJson);
+			System.out.println(poiJson);
+			System.out.println(poiJson1.toJSONString());
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		return 1;
 	}
 	
+	public static void main(String args[]){
 	
+		CustomPoiVO customPoiVO = new CustomPoiVO();
+		customPoiVO.setLat(10.555);
+		customPoiVO.setLon(22.55);
+		customPoiVO.setName("abc");
+		
+		CustomPoiUtil.addPoi(customPoiVO);
+	}
 }
--- src/com/custompoi/CustomPoiVO.java
+++ src/com/custompoi/CustomPoiVO.java
@@ -2,7 +2,25 @@ package com.custompoi;
 
 public class CustomPoiVO {
 	
-	public int addPoi(CustomPoiVO customPoiVO){
-		return 0;
+	private String name;
+	private double lat;
+	private double lon;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getLat() {
+		return lat;
+	}
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+	public double getLon() {
+		return lon;
+	}
+	public void setLon(double lon) {
+		this.lon = lon;
 	}
 }
